ead0f8a489f59327f362753e829b57ae96dc88a6
johntran <tranjohn1211@gmail.com>
2022-06-28 17:09:00 -0700

    work on the tab section

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,10 +4,30 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.getElementById('date')
+date.innerHTML = new Date().getFullYear()
 // ********** close links ************
-
+const navToggle = document.querySelector('.nav-toggle')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
 // ********** fixed navbar ************
+navToggle.addEventListener('click', function() {
+    const containerHeight = linksContainer.getBoundingClientRect().height
+    const linksHeight = links.getBoundingClientRect().height
 
+    if(containerHeight ===0) {
+        linksContainer.style.height = `${linksHeight}px`
+    } else {
+        linksContainer.style.height = 0
+    }
+})
+
+const navbar = document.getElementById('nav')
+const topLink = document.querySelector(".top-link")
+
+window.addEventListener("scroll", function () {
+    window.pageYOffset
+})
 // ********** smooth scroll ************
 // select links
+
